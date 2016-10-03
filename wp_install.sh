9ab91cdd9520a3e10a086a4bcb180f41929e93e2
Matt <matiasvarone@gmail.com>
2016-10-03 14:22:32 -0300

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -1,14 +1,10 @@
-# Add to ~/.bash_profile
-
-wp_install() {
-	latest="http://wordpress.org/latest.zip"
-	curl -O $latest 
-	unzip latest.zip
-	rm -rf __MACOSX latest.zip
-	cp -rf ./wordpress/* ./
-	rm -rf ./wordpress/ ./wp-content/plugins/hello.php ./readme.html
-	mkdir ./wp-content/uploads/
-	mv wp-config-sample.php wp-config.php
-	touch htaccess.txt robots.txt
-	open https://api.wordpress.org/secret-key/1.1/salt/
-}
+latest="http://wordpress.org/latest.zip"
+curl -O $latest 
+unzip latest.zip
+rm -rf __MACOSX latest.zip #osx only
+cp -rf ./wordpress/* ./
+rm -rf ./wordpress/ ./wp-content/plugins/hello.php ./readme.html
+mkdir ./wp-content/uploads/
+mv wp-config-sample.php wp-config.php
+touch htaccess.txt robots.txt
+open https://api.wordpress.org/secret-key/1.1/salt/
